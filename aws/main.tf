b4cb7c02cd4be7ac59e59083bc5f9f57483b04f3
ejcondorf88 <eliancondor2523658@gmail.com>
2026-02-06 02:23:51 -0500

    feat: Implement initial AWS EC2 instance provisioning with Docker and application deployment using Terraform.

--- aws/main.tf
+++ aws/main.tf
@@ -1,4 +1,17 @@
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.instance_name}-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+}
+
+resource "local_sensitive_file" "private_key" {
+  content  = tls_private_key.ec2_key.private_key_pem
+  filename = "${path.module}/${var.instance_name}-key.pem"
+}
 
 resource "aws_security_group" "web_sg" {
   name        = "allow_web_traffic"
@@ -29,6 +42,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  key_name      = aws_key_pair.generated_key.key_name
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   user_data              = templatefile("${path.module}/user_data.sh", {
@@ -43,4 +57,9 @@ resource "aws_instance" "web_server" {
 
 output "instance_public_ip" {
   value = aws_instance.web_server.public_ip
+}
+
+output "private_key" {
+  value     = tls_private_key.ec2_key.private_key_pem
+  sensitive = true
 }
