bc98f87887ac252aebc04d07cece77b6adf44a4f
ejcondorf88 <eliancondor2523658@gmail.com>
2026-02-06 04:11:43 -0500

    test

--- producer-api/src/main/java/com/sofkianos/producer/config/WebConfig.java
+++ producer-api/src/main/java/com/sofkianos/producer/config/WebConfig.java
@@ -14,14 +14,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "http://127.0.0.1:5173",
-                                "https://test3-vvxg.vercel.app",
-                                "https://test3-vvxg-git-release-100-ejcondorf88s-projects.vercel.app",
-                                "https://test3-vvxg-kk9qpfwth-ejcondorf88s-projects.vercel.app"
+                        .allowedOriginPatterns(
+                                "http://localhost:[*]",
+                                "http://127.0.0.1:[*]",
+                                "https://test3-vvxg*.vercel.app"
                         )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
